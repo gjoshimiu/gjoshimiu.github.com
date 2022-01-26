85acde3f340b29d38401faee5f00278aaf6573f9
ganeshjoshi <ganeshjoshi2070@gmail.com>
2022-01-25 19:57:01 -0600

    fix: refactor

--- js/fifteen.js
+++ js/fifteen.js
@@ -1,36 +1,36 @@
 (function ($) {
     $(document).ready(function () {
-        $("#puzzlearea").append("<div></div>");
+
 
         let emptyDiv = {x: 400, y: 300};
 
         $("#puzzlearea").find("div").each(function (i, e) {
-            var div = e;
-
-            // calculate x and y for this piece
-            var x = ((i % 4) * 100);
-            var y = (Math.floor(i / 4) * 100);
+            let div = e;
+            let x = ((i % 4) * 100);
+            let y = (Math.floor(i / 4) * 100);
 
             // set basic style and background
-            div.className = "puzzlepiece";
             div.style.left = x + 'px';
             div.style.top = y + 'px';
+
+            div.className = "puzzlepiece";
             div.style.backgroundImage = 'url("img/background.jpeg")';
             div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-
             // store x and y for later
             div.x = x;
             div.y = y;
         });
 
+        $("#puzzlearea").append("<div style='border: none'></div>");
+
         $("#shufflebutton").click(function () {
             let list = $("#puzzlearea").find("div");
             shuffle(list).each(function (i, e) {
-                var div = e;
+                let div = e;
 
                 // calculate x and y for this piece
-                var x = ((i % 4) * 100);
-                var y = (Math.floor(i / 4) * 100);
+                let x = ((i % 4) * 100);
+                let y = (Math.floor(i / 4) * 100);
 
                 // set basic style and background
                 div.className = "puzzlepiece";
@@ -50,34 +50,34 @@
             $(ee.target).addClass("movablepiece");
         }).mouseleave(function (ee) {
             $(ee.target).addClass("movablepiece");
-        }).click(function (idx) {
-            if (idx.target.innerText) {
-                let originalIndexX = idx.target.x;
-                let originalIndexY = idx.target.y;
+        }).click(function (currentElement) {
+            if (currentElement.target.innerText) {
+                let originalIndexX = currentElement.target.x;
+                let originalIndexY = currentElement.target.y;
 
                 const neighbour = findNeighbour(originalIndexX, originalIndexY);
                 const ab = neighbour.filter(a => {
                     return a.x === emptyDiv.x && a.y === emptyDiv.y;
                 });
                 if (ab.length > 0) {
-                    $("#puzzlearea").find("div").each(function (idx1, eee) {
-                        if (eee.x === ab[0].x && eee.y === ab[0].y) {
-                            console.log(eee, idx);
-                            const originalTop = idx.target.style.top;
-                            const originalLeft = idx.target.style.left;
+                    $("#puzzlearea").find("div").each(function (idx1, emptyElement) {
+                        if (emptyElement.x === ab[0].x && emptyElement.y === ab[0].y) {
+                            console.log(emptyElement, currentElement);
+                            const originalTop = currentElement.target.style.top;
+                            const originalLeft = currentElement.target.style.left;
 
-                            idx.target.x = eee.x;
-                            idx.target.y = eee.y;
-                            idx.target.style.top = eee.style.top;
-                            idx.target.style.left = eee.style.left;
+                            currentElement.target.x = emptyElement.x;
+                            currentElement.target.y = emptyElement.y;
+                            currentElement.target.style.top = emptyElement.style.top;
+                            currentElement.target.style.left = emptyElement.style.left;
 
-                            eee.style.top = originalTop;
-                            eee.style.left = originalLeft;
-                            eee.x = originalIndexX;
-                            eee.y = originalIndexY;
+                            emptyElement.style.top = originalTop;
+                            emptyElement.style.left = originalLeft;
+                            emptyElement.x = originalIndexX;
+                            emptyElement.y = originalIndexY;
 
-                            emptyDiv.x = eee.x;
-                            emptyDiv.y = eee.y;
+                            emptyDiv.x = emptyElement.x;
+                            emptyDiv.y = emptyElement.y;
                         }
                     });
                 }
@@ -111,7 +111,6 @@
                 array[i] = array[j];
                 array[j] = temp;
             }
-            console.log(array);
             return array
         }
 
